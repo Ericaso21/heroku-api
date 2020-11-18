0654aee152df9c85518dcd1b1660213430302313
Eric Solarte <essolarte1@misena.edu.co>
2020-11-18 10:27:34 -0500

    slim-api

---
+++ backend/config/config.php
@@ -0,0 +1,20 @@
+<?php
+    const BASE_URL = "http://localhost:8080/parkingsoft/";
+
+    //Zona horaria
+    date_default_timezone_set('America/Bogota');
+
+    //Datos conexion a base de datos
+    const DB_HOST = "bosmx1gq7jgtntmnjihe-mysql.services.clever-cloud.com";
+    const DB_NAME = "bosmx1gq7jgtntmnjihe";
+    const DB_USER = "u9jkvrvrq2vyb4hw";
+    const DB_PASSWORD = "U3gNV3DvBaroJ9dLLvab";
+    const DB_CHARSET = "charset=utf8";
+
+    //Deliminador de decimal y millar
+    const SPD = ",";
+    const SPM = ".";
+
+    //SIMBOLO DE MONEDA
+    const SMONEY = "$";
+?>
